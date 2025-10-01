85c88f8d5feed8c530f5d3959172f8200eb42694
Nazar-e-Imam <youremail@example.com>
2025-10-02 02:49:13 +0500

    Clean up project structure and update globe splash screen

--- gallery/layout.jsx
+++
@@ -1,6 +0,0 @@
-import "../globals.css"
-import "./gallery-styles.css"
-
-export default function GalleryLayout({ children }) {
-  return <>{children}</>
-}
--- lib/screens/globe_splash_screen.dart
+++ lib/screens/globe_splash_screen.dart
@@ -1,4 +1,4 @@
-Rimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import '../ui/screens/auth/login_screen.dart';
